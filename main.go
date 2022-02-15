56c3943dfa08d80f59048e991a3cb410190320f2
Dave Johnson <dave@secondshutter.com>
2022-02-14 19:43:32 -0600

    cleaning initial code.  move add/remove client to separate functions.

--- main.go
+++ main.go
@@ -10,15 +10,27 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"net"
 	"os"
-
-	"time"
 )
 
 // list of connected clients
 var clients = []net.Conn{}
+
+func addClient(c net.Conn) {
+	fmt.Println("adding client:", c.RemoteAddr().String())
+	clients = append(clients, c)
+}
+
+func removeClient(c net.Conn) {
+	fmt.Println("removing client:", c.RemoteAddr().String())
+	for i, _ := range clients {
+		if clients[i] == c {
+			clients = append(clients[:i], clients[i+1:]...)
+			break
+		}
+	}
+}
 
 func broadcast(msg string, from net.Conn) {
 	for _, c := range clients {
@@ -30,24 +42,12 @@ func broadcast(msg string, from net.Conn) {
 }
 
 func handleConnection(c net.Conn) {
-	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	addClient(c)
 
-	clients = append(clients, c)
-	fmt.Println("appending client: ", len(clients))
-
-	fmt.Printf("net.conn: %v\n", c)
 	for {
 		netData, err := bufio.NewReader(c).ReadString('\n')
 		if err != nil {
-			fmt.Printf("closing some client", err)
-			for i, _ := range clients {
-				if clients[i] == c {
-					fmt.Println("removing client", len(clients))
-					clients = append(clients[:i], clients[i+1:]...)
-					fmt.Println("removed", len(clients))
-					break
-				}
-			}
+			removeClient(c)
 			c.Close()
 			return
 		}
@@ -70,14 +70,17 @@ func main() {
 		return
 	}
 	defer l.Close()
-	rand.Seed(time.Now().Unix())
 
+	fmt.Println("Staring TCP Chat on port", PORT)
 	for {
+		// connect to new clients
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
+		// thread the process handler
 		go handleConnection(c)
 	}
 }
